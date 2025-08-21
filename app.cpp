f2a8b15b207aaa70215acdbc2dfa440734502361
ornaras <ornaras.us@gmail.com>
2025-08-21 10:16:07 +0400

    Удаление избыточных аргументов функций

--- app.cpp
+++ app.cpp
@@ -1,10 +1,5 @@
 ﻿#include "generator.cpp"
-#include "utils.cpp"
 
 int main() {
-	int sizeDict{ inputDigit("Input dictionary's size", 75)};
-	int lenPasswd{ random(7, 16) };
-
-	std::string dict{ genDictionary(sizeDict) };
-	std::string passwd{ genPassword(lenPasswd, dict) };
+	std::string passwd{ genPassword() };
 }
--- generator.cpp
+++ generator.cpp
@@ -1,5 +1,6 @@
 #include <random>
 #include <iostream>
+#include "utils.cpp"
 
 const std::string SYMBOLS[]{
 	"qwertyuiopasdfghjklzxcvbm",
@@ -12,26 +13,29 @@ static int random(int min, int max) {
 	std::random_device dev;
 	std::mt19937 rng(dev());
 	std::uniform_int_distribution<std::mt19937::result_type>
-		dist(min, max - 1);
+		dist(min, max);
 	return dist(rng);
 }
 
-static std::string genDictionary(int size) {
+static std::string genDictionary() {
+	int size{ inputDigit("Input dictionary's size", 75) };
 	std::string dict;
 	for (int i{ 0 }; i < size; i++) {
-		std::string characters{ SYMBOLS[random(0, std::size(SYMBOLS))] };
-		std::string symbol(1, characters[random(0, std::size(characters))]);
+		std::string characters{ SYMBOLS[random(0, std::size(SYMBOLS) - 1)] };
+		std::string symbol(1, characters[random(0, std::size(characters) - 1)]);
 		dict.append(symbol);
 	}
 	std::cout << "Dictionary: " << dict << std::endl;
 	return dict;
 }
 
-static std::string genPassword(int len, std::string dict) {
+static std::string genPassword() {
+	std::string dict{ genDictionary() };
+	int len{ random(7, 15) };
 	std::string passwd;
 
 	for (int i{ 0 }; i < len; i++) {
-		std::string symbol(1, dict[random(0, std::size(dict))]);
+		std::string symbol(1, dict[random(0, std::size(dict) - 1)]);
 		passwd.append(symbol);
 	}
 	std::cout << "Password: " << passwd << std::endl;
