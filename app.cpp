8cd286d20b52dabde6c58a2a5ccc23c35bbc46b5
ornaras <ornaras.us@gmail.com>
2025-08-22 11:14:06 +0400

    Реализовано ввод пароля с его проверкой

--- app.cpp
+++ app.cpp
@@ -1,6 +1,18 @@
 ﻿#include "generator.h"
+#include "constants.h"
 #include "utils.h"
 
 int main() {
 	std::string passwd{ genPassword() };
+	for (int i{ 0 }; i < MAX_COUNT_TRIES;i++) {
+		std::cout << std::endl;
+		std::string input{ inputString("Enter a password") };
+		if (input == passwd) {
+			std::cout << "You win!" << std::endl;
+			return 0;
+		}
+		else {
+			std::cout << "Wrong password!" << std::endl;
+		}
+	}
 }
--- constants.h
+++ constants.h
@@ -3,3 +3,4 @@
 const unsigned MIN_SIZE_PASSWORD = 7;
 const unsigned MAX_SIZE_PASSWORD = 15;
 const unsigned DEFAULT_SIZE_DICT = 75;
+const unsigned MAX_COUNT_TRIES = 5;
