48300aa089c6a2f588340944f562b6d13c105ed2
ornaras <ornaras.us@gmail.com>
2025-08-22 11:06:19 +0400

    Переработан ввод числа

--- utils.cpp
+++ utils.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <sstream>
 
 static int inputDigit(std::string text, int defaultValue) {
-
 	std::cout << text << " [" << defaultValue << "]: ";
-	int value;
-	if (std::cin.peek() == '\n' || !(std::cin >> value)) {
+	std::string raw{ "" };
+	std::getline(std::cin, raw);
+	try {
+		return std::stoi(raw);
+	}
+	catch (const std::invalid_argument& e) {
 		return defaultValue;
 	}
-	return value;
+	catch (const std::out_of_range& e) {
+		return defaultValue;
+	}
 }
